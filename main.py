a62085f06a63637b6dac8dbcdcab4331f5e75e3f
Lilac0000 <140547328+Lilac0000@users.noreply.github.com>
2025-07-21 20:25:23 -0400

    Create a RESTful API using FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import os
-
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
@@ -14,9 +13,7 @@ class Data(BaseModel):
     fnlgt: int = Field(..., example=178356)
     education: str = Field(..., example="HS-grad")
     education_num: int = Field(..., example=10, alias="education-num")
-    marital_status: str = Field(
-        ..., example="Married-civ-spouse", alias="marital-status"
-    )
+    marital_status: str = Field(..., example="Married-civ-spouse", alias="marital-status")
     occupation: str = Field(..., example="Prof-specialty")
     relationship: str = Field(..., example="Husband")
     race: str = Field(..., example="White")
@@ -26,31 +23,25 @@ class Data(BaseModel):
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
 
-path = None # TODO: enter the path for the saved encoder 
-encoder = load_model(path)
+# ✅ TODO: Provide paths to saved models
+encoder = load_model("model/encoder.pkl")
+model = load_model("model/model.pkl")
 
-path = None # TODO: enter the path for the saved model 
-model = load_model(path)
+# ✅ TODO: Create a FastAPI app
+app = FastAPI()
 
-# TODO: create a RESTful API using FastAPI
-app = None # your code here
-
-# TODO: create a GET on the root giving a welcome message
+# ✅ TODO: GET request for welcome message
 @app.get("/")
 async def get_root():
-    """ Say hello!"""
-    # your code here
-    pass
+    """Say hello!"""
+    return {"message": "Welcome to the income classifier API!"}
 
-
-# TODO: create a POST on a different path that does model inference
+# ✅ TODO: POST request for inference
 @app.post("/data/")
 async def post_inference(data: Data):
     # DO NOT MODIFY: turn the Pydantic model into a dict.
     data_dict = data.dict()
     # DO NOT MODIFY: clean up the dict to turn it into a Pandas DataFrame.
-    # The data has names with hyphens and Python does not allow those as variable names.
-    # Here it uses the functionality of FastAPI/Pydantic/etc to deal with this.
     data = {k.replace("_", "-"): [v] for k, v in data_dict.items()}
     data = pd.DataFrame.from_dict(data)
 
@@ -64,11 +55,13 @@ async def post_inference(data: Data):
         "sex",
         "native-country",
     ]
+
+    # ✅ TODO: Process data for inference
     data_processed, _, _, _ = process_data(
-        # your code here
-        # use data as data input
-        # use training = False
-        # do not need to pass lb as input
+        data, categorical_features=cat_features, training=False, encoder=encoder
     )
-    _inference = None # your code here to predict the result using data_processed
+
+    # ✅ TODO: Make prediction
+    _inference = inference(model, data_processed)
+
     return {"result": apply_label(_inference)}
